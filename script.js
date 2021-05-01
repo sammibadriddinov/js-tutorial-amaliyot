79d160adfaf7e4d9b4e297c32f78609d5968bada
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-05-01 09:11:30 +0500

    start practise

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Darsga topshiriq:
+
+1) o'zgaruvchan numberOfFilms yarating va foydalanuvchidan savolga javobini qo'ying:
+"Siz qancha film ko'rdingiz?"
+
+2) personalMovieDB obyektini yarating va unga quyidagi xususiyatlarni qo'ying:
+- count - birinchi savolga javob shu erda beriladi
+- movies - bu ob'ektga bo'sh ob'ektni qo'ying
+- actors - shuningdek, bo'sh narsani joylashtiradilar
+- genres - bu erda bo'sh massivni qo'ying
+- privat - bu xususiyatga mantiqiy (boolean) false qiymatini qo'ying
+
+3) foydalanuvchiga ikkita savol bering:
+  - "Oxirgi ko'rgan filmlaringizdan biri?"
+  - 'Unga qancha baho bergan bo'lar edingiz?'
+Javoblar alohida o'zgaruvchilarga joylashtirilishi kerak.
+Filmlar ob'ektiga javoblarni quyidagi shaklda yozing:
+  filmlar: {
+    'logan': '8.1'
+  }
+
+Konsolda hamma narsa xatosiz ishlashini tekshiring */
+
+"use strict";
