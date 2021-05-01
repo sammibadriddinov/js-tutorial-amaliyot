87c4240a861a802df32d9e729b37c92c93dc0de5
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-05-01 09:31:32 +0500

    end commit

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 /* Darsga topshiriq:
 
-1) o'zgaruvchan numberOfFilms yarating va foydalanuvchidan savolga javobini qo'ying:
-"Siz qancha film ko'rdingiz?"
+1) o'zgaruvchan numberOfNews yarating va foydalanuvchidan savolga javobini qo'ying:
+"Siz qancha yangilik ko'rdingiz?"
 
-2) personalMovieDB obyektini yarating va unga quyidagi xususiyatlarni qo'ying:
+2) personalNewsDB obyektini yarating va unga quyidagi xususiyatlarni qo'ying:
 - count - birinchi savolga javob shu erda beriladi
-- movies - bu ob'ektga bo'sh ob'ektni qo'ying
-- actors - shuningdek, bo'sh narsani joylashtiradilar
+- news - bu ob'ektga bo'sh ob'ektni qo'ying
+- actors - shuningdek, bo'sh ob'ekt joylashtiradilar
 - genres - bu erda bo'sh massivni qo'ying
 - privat - bu xususiyatga mantiqiy (boolean) false qiymatini qo'ying
 
 3) foydalanuvchiga ikkita savol bering:
-  - "Oxirgi ko'rgan filmlaringizdan biri?"
+  - "Oxirgi ko'rgan yangiliklarizdan biri?"
   - 'Unga qancha baho bergan bo'lar edingiz?'
 Javoblar alohida o'zgaruvchilarga joylashtirilishi kerak.
-Filmlar ob'ektiga javoblarni quyidagi shaklda yozing:
-  filmlar: {
-    'logan': '8.1'
+Yangiliklar ob'ektiga javoblarni quyidagi shaklda yozing:
+  news: {
+    'BBC': '8.1'
   }
 
 Konsolda hamma narsa xatosiz ishlashini tekshiring */
 
 "use strict";
+
+const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+
+const personalNewsDB = {
+  count: numberOfNews,
+  news: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
+  b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+
+personalNewsDB.news[a] = b;
+console.log(personalNewsDB);
